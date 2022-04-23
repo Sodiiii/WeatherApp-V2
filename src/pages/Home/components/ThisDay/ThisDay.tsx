88186af0227e9b81f4f0259da4f1d10697977ae6
Vlad Konkin <sodii@S1mple-Air.N4>
2022-04-23 15:33:47 +1000

    react/landing

---
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import s from './ThisDay.module.scss'
+import { GlobalSvgSelector } from '../../../../assets/icons/global/GlobalSvgSelector';
+
+
+interface Props  { 
+
+}
+
+export const ThisDay = (props: Props) => {
+ 
+  return( 
+        <div className={s.this__day}>
+            <div className={s.top__block}>
+                <div className={s.top__block__wrapper}>
+                    <div className={s.this__temp}>20°</div>
+                    <div className={s.this__day__name}>Сегодня</div>
+                </div>
+                <GlobalSvgSelector id = 'sun' />
+            </div>
+            <div className={s.bottom__block}>
+                <div className={s.this__time}>
+                    Время: <span>21:54</span>
+                </div>
+                <div className={s.this__city}>
+                <span>Санкт-Петербург</span>
+                </div>
+            </div>
+
+        </div>
+    )
+  
+};
